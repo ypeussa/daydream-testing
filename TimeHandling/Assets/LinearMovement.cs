dcfb4f46aae90bd839af5e7a0342867f0e92821a
Yrjö Peussa <peussa@iki.fi>
2017-09-10 19:49:37 +0300

    separate project with frame timing tests

---
+++ TimeHandling/Assets/LinearMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LinearMovement : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public Timing timing;
+    public enum Timing { Update, FixedUpdate, LateUpdate};
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (timing == Timing.Update)
+            transform.position += Time.deltaTime * speed * Vector3.forward;	
+	}
+
+    void LateUpdate() {
+        if (timing == Timing.LateUpdate)
+            transform.position += Time.deltaTime * speed * Vector3.forward;
+
+    }
+    void FixedUpdate() {
+        if (timing == Timing.FixedUpdate)
+            transform.position += Time.deltaTime * speed * Vector3.forward;
+    }
+}
---
+++ TimeHandling/Assets/MovingCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCharacter : MonoBehaviour {
+    public float speed = 5.0f;
+    public Transform camera;
+
+    Vector3 toCamera;
+
+    private void Start() {
+        toCamera = camera.position - transform.position; 
+    }
+    void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        camera.transform.position = transform.position + toCamera;
+    }
+}
