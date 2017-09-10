ad34611252f69de2e384b57307a5c7c9d468b8eb
Yrjö Peussa <peussa@iki.fi>
2017-09-10 20:51:31 +0300

    door trigger changes

--- unity-basics/Assets/Scripts/DoorTrigger.cs
+++ unity-basics/Assets/Scripts/DoorTrigger.cs
@@ -4,46 +4,24 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour {
 
-	public Transform door;
-	public Transform openPoint;
+	public SlidingDoor door;
+    Collider player;
 
-	public float speed;
+    void Start() {
+        player = FindObjectOfType<PlayerController>().GetComponentInChildren<Collider>();
+        if (!player)
+            Debug.LogError("couldn't find an object in the scene with a PlayerController and collider");
+    }
 
-	Vector3 dir;
-	float dist;
-	Vector3 statePos;
-
-	Collider player;
-
-	void Start () {
-		player = FindObjectOfType<PlayerMover>().GetComponentInChildren<Collider>();
-		statePos = transform.position;
-	}
-
-	void Update () {
-		if (dist > 0) {
-			door.position += dir * speed * Time.deltaTime;
-			dist -= speed * Time.deltaTime;
-		} else {
-			door.position = statePos;
-		}
-	}
-
-	void OnTriggerEnter (Collider c) {
+    void OnTriggerEnter (Collider c) {
 		if (c == player) {
-			//door.position = openPoint.position;
-			dir = (openPoint.position - door.position).normalized;
-			dist = Vector3.Distance(openPoint.position, door.position);
-			statePos = openPoint.position;
+            door.Open();
 		}
 	}
 
 	void OnTriggerExit (Collider c) {
 		if (c == player) {
-			//door.position = transform.position;
-			dir = (transform.position - door.position).normalized;
-			dist = Vector3.Distance(transform.position, door.position);
-			statePos = transform.position;
-		}
+            door.Close();
+        }
 	}
 }
---
+++ unity-basics/Assets/Scripts/SlidingDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoor : MonoBehaviour {
+    public Transform doorObject;
+    public Transform openPosition;
+    public float speed;
+
+    // SerializeField makes a private variable visible & editable in Editor UI as if it was public
+    [SerializeField]
+    bool open;
+
+    Vector3 closedPosition;
+
+    public void Open() {
+        open = true;
+    }
+
+    public void Close() {
+        open = false;
+    }
+
+    void Awake() {
+        closedPosition = doorObject.position;
+    }
+
+    void Update() {
+        var destination = open ? openPosition.position : closedPosition;
+        doorObject.position = Vector3.MoveTowards(doorObject.position, destination, Time.deltaTime * speed);
+    }
+}
