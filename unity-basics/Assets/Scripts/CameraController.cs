7f719ebe7fa53f591b29b350e556f7918ea24961
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2017-09-10 21:34:27 +0300

    world space buttons

---
+++ unity-basics/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform player;
+
+	Vector3 startPos;
+	bool followingPlayer;
+
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	void Update () {
+		if (followingPlayer) {
+			var newPos = player.position;
+			newPos.y = startPos.y;
+			transform.position = newPos;
+		} else {
+			transform.position = startPos;
+		}
+	}
+
+	public void FollowPlayer () {
+		followingPlayer = true;
+	}
+
+	public void CenterCamera () {
+		followingPlayer = false;
+	}
+}
