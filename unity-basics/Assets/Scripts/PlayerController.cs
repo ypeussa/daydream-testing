04723848bf6cf9c0bbada7e6ddaa7914035c5613
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2017-09-10 22:01:53 +0300

    UI blocks raycast

--- unity-basics/Assets/Scripts/PlayerController.cs
+++ unity-basics/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour {
 
 	public LayerMask mask;
 	PlayerMover mover;
-
+	EventSystem _eventSystem;
 	void Start () {
 		mover = GetComponent<PlayerMover>();
+		_eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
 	}
 
 	void Update () {
@@ -16,8 +18,7 @@ public class PlayerController : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
-			if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask)) {
-
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask) && !_eventSystem.IsPointerOverGameObject()) {
 				mover.MoveToPoint(hit.point);
 			}
 		}
